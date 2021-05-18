d70c9bb80f7c1e97eaf76fc678601ddb0030f6fb
Ashton Huxtable <aehuxtable@gmail.com>
2021-05-18 13:53:59 -0600

    Add beforeEach to repository tests

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,48 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
+import Recipe from '../src/classes/Recipe'
 
-describe('RecipeRespository', () => {
+describe('RecipeRepository', () => {
+  let repository, recipe;
+
+  beforeEach(() => {
+    repository = new RecipeRepository(recipe);
+    recipe = new Recipe(  "id": 595736,
+      "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
+      "ingredients": [
+        {
+          "id": 20081,
+          "quantity": {
+            "amount": 1.5,
+            "unit": "c"
+          }
+        },
+        {
+          "id": 18372,
+          "quantity": {
+            "amount": 0.5,
+            "unit": "tsp"
+          }
+        }],
+      "instructions": [
+        {
+          "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
+          "number": 1
+        },
+        {
+          "instruction": "Add egg and vanilla and mix until combined.",
+          "number": 2
+        }],
+      "name": "Loaded Chocolate Chip Pudding Cookie Cups",
+      "tags": [
+        "antipasti",
+        "starter",
+        "snack"]);
+  });
+
   it('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+
+
 })
