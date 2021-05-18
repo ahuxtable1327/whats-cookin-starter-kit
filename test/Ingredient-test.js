ebead5917df6439fcfe4068ba06bdbae1abe9078
Ashton Huxtable <aehuxtable@gmail.com>
2021-05-17 19:31:07 -0600

    Add tests to check for function and instance of ingredient

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -1,2 +1,12 @@
 import { expect } from 'chai';
 import Ingredient from '../src/classes/Ingredient';
+
+describe('Ingredient', () => {
+
+  it('should be a function', () =>{
+    expect(Ingredient).to.be.a('function')
+  });
+
+  it('should be an instance of Ingredient' () => {
+    expect(ingredient).to.be.an.instanceof(Ingredient)
+  });
