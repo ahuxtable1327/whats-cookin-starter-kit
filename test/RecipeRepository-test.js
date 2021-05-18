c7b36baaaeaeb179413269d8cfc4975fea0e0198
Ashton Huxtable <aehuxtable@gmail.com>
2021-05-18 13:55:28 -0600

    Add tests to check for filtered lists

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -44,5 +44,26 @@ describe('RecipeRepository', () => {
     expect(RecipeRepository).to.be.a('function');
   });
 
+  it('should be an instance of RecipeRepository', () => {
+    expect(repository).to.be.an.instanceof(RecipeRepository);
+  });
 
+  it('should be able to take in a recipe', () => {
+    expect(repository.recipe).to.equal(recipe);
+  });
+
+  it('should return a filtered list based on recipe tags', () => {
+    let recipeList = recipe.filterByTag('starter');
+    expect(recipeList.length).to.equal(1);
+  });
+
+  it('should return a filtered list based on recipe name', () => {
+    let recipeList = recipe.filterByName('pork chops');
+    expect(recipeList.length).to.equal(0);
+  });
+
+  it('should return a filtered list based on ingredients', () => {
+    let recipeList = recipe.filterByIngredient('wheat flour');
+    expect(recipeList.length).to.equal(1);
+  });
 })
