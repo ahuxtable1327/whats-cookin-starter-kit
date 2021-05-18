6c8efe1f6bcd2e386e9f051c7ee3bc86aaf283db
Ashton Huxtable <aehuxtable@gmail.com>
2021-05-17 19:32:03 -0600

    Add tests to check for ingredient name and cost in cents

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -10,3 +10,15 @@ describe('Ingredient', () => {
   it('should be an instance of Ingredient' () => {
     expect(ingredient).to.be.an.instanceof(Ingredient)
   });
+
+  it('should have a name', () => {
+    let ingredient = new Ingredient('flour');
+    expect(ingredient.name).to.equal('flour');
+  });
+
+  it('should have a cost in cents', () => {
+    let ingredient = new Ingredient('flour', 250);
+    expect(ingredient.estimatedCostInCents).to.equal(250);
+  });
+
+})
