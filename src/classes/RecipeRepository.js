d05b6e2970055bfb3f3e173dfaea0bc8222bb672
Ashton Huxtable <aehuxtable@gmail.com>
2021-05-18 19:39:37 -0600

    Add test recipe file with data

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,11 +1,15 @@
 class RecipeRepository {
   constructor(recipes) {
-    console.log(recipes)
     this.recipes = recipes;
+    console.log(this.recipes)
   }
   filterByTag(tag) {
 
   }
+  // return a filtered array of recipes based on the tags
+  // my tags are an array within the recipe object
+  // return any recipe that has a tag that includes the search
+  // 
 }
 
 export default RecipeRepository;
--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,59 +1,28 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
 import Recipe from '../src/classes/Recipe'
+import testRecipes from './testRecipeData'
 
 describe('RecipeRepository', () => {
-  let repository, recipe;
+  let repository, recipes
 
   beforeEach(() => {
-    let puddingCup = {"id": 595736,
-      "image": "https://spoonacular.com/recipeImages/595736-556x370.jpg",
-      "ingredients": [
-        {
-          "id": 20081,
-          "quantity": {
-            "amount": 1.5,
-            "unit": "c"
-          }
-        },
-        {
-          "id": 18372,
-          "quantity": {
-            "amount": 0.5,
-            "unit": "tsp"
-          }
-        }],
-      "instructions": [
-        {
-          "instruction": "In a large mixing bowl, whisk together the dry ingredients (flour, pudding mix, soda and salt). Set aside.In a large mixing bowl of a stand mixer, cream butter for 30 seconds. Gradually add granulated sugar and brown sugar and cream until light and fluffy.",
-          "number": 1
-        },
-        {
-          "instruction": "Add egg and vanilla and mix until combined.",
-          "number": 2
-        }],
-      "name": "Loaded Chocolate Chip Pudding Cookie Cups",
-      "tags": [
-        "antipasti",
-        "starter",
-        "snack"]};
-      recipe = new Recipe(puddingCup);
-      repository = new RecipeRepository(recipe);
+      repository = new RecipeRepository(testRecipes);
   });
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
 
-  it.skip('should be an instance of RecipeRepository', () => {
+  it('should be an instance of RecipeRepository', () => {
     expect(repository).to.be.an.instanceof(RecipeRepository);
   });
 
-  it.skip('should be able to take in a recipe', () => {
-    expect(repository.recipe).to.equal(recipe);
+  it('should be able to take in a recipe', () => {
+    expect(repository.recipes).to.equal(recipe);
   });
 
-  it.skip('should return a filtered list based on recipe tags', () => {
+  it('should return a filtered list based on recipe tags', () => {
     let recipeList = recipe.filterByTag('starter');
     expect(recipeList.length).to.equal(1);
   });
