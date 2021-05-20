7d9ee16628de3442d811a9a6f3735b59de59920f
Ashton Huxtable <aehuxtable@gmail.com>
2021-05-20 14:12:50 -0600

    Add tests to check for filtered favorites

--- test/User-test.js
+++ test/User-test.js
@@ -1,10 +1,17 @@
 import { expect } from 'chai';
 import User from '../src/classes/User'
+import RecipeRepository from '../src/classes/RecipeRepository';
+import testRecipes from './testRecipeData'
 
 
 
 describe('User', () => {
-  let user;
+  let user, repository;
+
+  beforeEach(() => {
+    repository = new Repository(testRecipes);
+    user = new
+  })
 
   it.skip('should be a function', () => {
     expect(User).to.be.a('function');
@@ -16,13 +23,26 @@ describe('User', () => {
 
   it.skip('should be able to favorite a recipe', () => {
     expect(user.favoriteRecipes.length).to.equal(2);
-    expect(user.favoriteRecipes).to.deep.equal([recipe1, recipe2]);
+    expect(user.favoriteRecipes).to.deep.equal([repository.recipes[0], repository.recipes[1]]);
   });
 
   it.skip('should be able to unfavorite a recipe', () => {
     expect(user.favoriteRecipes.length).to.equal(1);
-    expect(user.favoriteRecipes).to.deep.equal([recipe1]);
-  })
+    expect(user.favoriteRecipes).to.deep.equal([repository.recipes[0]]);
+  });
 
-  
+  it.skip('should be able to add to a meal plan', () => {
+    expect(user.mealPlans.length).to.equal(1);
+    expect(user.mealPlans).to.deep.equal([repository.recipes[1]]);
+  });
+
+  it.skip('should be able to filter favorited recipes by tags', () => {
+    user.filterFavoritesByTag('dinner');
+    expect(user.filteredFavorites).to.deep.equal([repository.recipes[1]]);
+  });
+
+  it.skip('should be able to filter favorited recipes by name', () => {
+    user.filterFavoritesByName('cookie');
+    expect(user.filteredFavorites).to.deep.equal([repository.recipes[0]]);
+  })
 })
