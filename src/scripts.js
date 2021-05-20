1c95c621b9c9d4870465e54ad68404dbc6c458f6
Beth <bethm.meeker@gmail.com>
2021-05-20 13:07:55 -0700

    start scripts.js for functionality

--- src/scripts.js
+++ src/scripts.js
@@ -3,46 +3,44 @@ import apiCalls from './apiCalls';
 import Recipe from '../src/classes/Recipe';
 import RecipeRepository from '../src/classes/RecipeRepository';
 import Ingredient from '../src/classes/Ingredient';
-import { recipeData } from '../src/data/recipes';
-import { ingredientsData } from '../src/data/ingredients';
-import { usersData } from '../src/data/users';
+// import { recipeData } from '../src/data/recipes';
+// import { ingredientsData } from '../src/data/ingredients';
+// import { usersData } from '../src/data/users';
 
 
-
-
-console.log('Hello world');
 //DOM variables
   //buttons and form
-const favoritesBtn = document.getElementByID('favBtn');
-const breakfastBtn = document.getElementByID('breakfastBtn');
-const lunchBtn = document.getElementByID('lunchBtn');
-const dinnerBtn = document.getElementByID('dinnerBtn');
-const dessertBtn = document.getElementByID('dessertBtn');
-const searchRecipeForm = document.getElementByID('search');
-const addToMPBtn = document.getElementByID('addToMPBtn');
+const favoritesBtn = document.getElementById('favBtn');
+const breakfastBtn = document.getElementById('breakfastBtn');
+const lunchBtn = document.getElementById('lunchBtn');
+const dinnerBtn = document.getElementById('dinnerBtn');
+const dessertBtn = document.getElementById('dessertBtn');
+const searchRecipeForm = document.getElementById('search');
+const addToMPBtn = document.getElementById('addToMPBtn');
 
   //page areas/sections
 //(grabbed section containing the divs)
-const randomRecArea = document.getElementByID('randomRecipes');
-const recipeByCat = document.getElementByID('recipeByCat');
-const imageDesc = document.getElementByID('imageDesc');
-const prepInstArea = document.getElementByID('prepInstArea');
-const favoritesArea = document.getElementByID('favoritesArea');
-const mealPlan = document.getElementByID('mealPlan');
-
+const randomRecArea = document.getElementById('randomRecipes');
+const recipeByCat = document.getElementById('recipeByCat');
+const imageDesc = document.getElementById('imageDesc');
+const prepInstArea = document.getElementById('prepInstArea');
+const favoritesArea = document.getElementById('favoritesArea');
+const mealPlan = document.getElementById('mealPlan');
 
 
 // Event Listeners
-// window.addEventListener('load', loadRandomUserRec);
+window.addEventListener('load', loadRandomInfo);
 // favoritesBtn.addEventListener('click', showFavRecipes);
-breakfastBtn.addEventListener('click', showBreakfastRecipes);
-lunchBtn.addEventListener('click', showLunchRecipes);
-dinnerBtn.addEventListener('click', showDinnerRecipes);
-dessertBtn.addEventListener('click', showDessertRecipes);
+// breakfastBtn.addEventListener('click', showBreakfastRecipes);
+// lunchBtn.addEventListener('click', showLunchRecipes);
+// dinnerBtn.addEventListener('click', showDinnerRecipes);
+// dessertBtn.addEventListener('click', showDessertRecipes);
 // searchRecipeForm.addEventListener('click', showAllRecipes);
-addToMPBtn.addEventListener('click', addRecipeToMealPlan);
+// addToMPBtn.addEventListener('click', addRecipeToMealPlan);
 
 
-loadRandomUserRec() {
-
+//functions
+function loadRandomInfo() {
+  console.log('Working');
+  // on window load, a random user should be logged in and some random recipes should load at bottom for 'Popular Recipes section';
 }
