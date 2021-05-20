7168be613924be2def1555fe2e3a9608872e10e1
Beth <bethm.meeker@gmail.com>
2021-05-20 12:26:44 -0700

    Start adding DOM variables for iteration1 functionality

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import apiCalls from './apiCalls';
 
-
-
-
 console.log('Hello world');
+//DOM variables
+const favoritesBtn = document.getElementByID('favBtn');
+const breakfastBtn = document
