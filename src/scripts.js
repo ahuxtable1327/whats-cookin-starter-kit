618349e693e34d5a127f705454e588116d5956f0
Beth <bethm.meeker@gmail.com>
2021-05-25 17:03:59 -0600

    Style delete button to overlay imgs in favorites

--- src/scripts.js
+++ src/scripts.js
@@ -217,8 +217,8 @@ function displayFavorites() {
   let filteredRecipes = favoriteRecipes.forEach(recipe => {
     favoritesArea.innerHTML += `
       <div class='recipe recipe-listing' id='${recipe.id}'>
-        <h3 class>${recipe.name}</h3>
         <button class='btn delete-btn' id='${recipe.id}'>🗑 Delete Recipe</button>
+        <h3 class='fav-name'>${recipe.name}</h3>
         <img class='recipe-img' src='${recipe.image}' alt='${recipe.name}'>
       </div>
   `
